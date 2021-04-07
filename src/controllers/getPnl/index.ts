55807f68747981342e8cd07b8dfee095469ce65d
Arjun Nemani <nemaniarjun@gmail.com>
2021-04-08 01:04:25 +0530

    render pnl cards as png

--- src/controllers/getPnl/index.ts
+++ src/controllers/getPnl/index.ts
@@ -1,9 +1,13 @@
 import { getRelevantPositionChangedEvents } from "./subgraph";
+import * as handlebars from "handlebars";
+import * as fs from "fs";
+import * as path from "path";
+import sharp from "sharp";
 
 const getPnl = async (req, res, next) => {
-  const trader: String = req.query.trader;
-  const amm: String = req.query.amm;
-  const blockNumber: String = req.query.blockNumber;
+  const trader: string = req.query.trader;
+  const amm: string = req.query.amm;
+  const blockNumber: string = req.query.blockNumber;
 
   if (!trader || !amm || !blockNumber) {
     const err = new Error("Required query params missing");
@@ -27,13 +31,35 @@ const getPnl = async (req, res, next) => {
   const finalPosition = Number(relevantEvents[1].positionSizeAfter) * 1e-18;
   const roi = (totalRealizedPnl / finalMargin) * 100;
 
-  res.json({
-    totalRealizedPnl,
-    roi,
-    entryPrice,
-    exitPrice,
-    positionSide: finalPosition > 0 ? "LONG" : "SHORT",
-  });
+  const green = "#40C87F";
+  const red = "#EA6262";
+  const template = handlebars.compile(
+    fs.readFileSync(path.join(__dirname, "./card.svg"), {
+      encoding: "utf8",
+      flag: "r",
+    })
+  );
+
+  const data = {
+    exitPrice: exitPrice.toFixed(1),
+    entryPrice: entryPrice.toFixed(1),
+    roi: roi.toFixed(1),
+    pair: "BTC/USDC",
+    roiColor: roi > 0 ? green : red,
+    positionSideColor: finalPosition > 0 ? green : red,
+    positionSide: finalPosition > 0 ? "LONG " : "SHORT ",
+  };
+
+  const resultSvg = template(data);
+  console.log(resultSvg);
+  
+  const resultSvgBuffer = Buffer.from(resultSvg);
+  const resultPng = await sharp(resultSvgBuffer).toFormat("png").toBuffer();
+  const resultBase64Png = `data:image/png;base64,${resultPng.toString('base64')}`
+  
+  console.log(resultBase64Png);
+  res.writeHead(200, { "Content-Type": "image/png", 'Content-Length': resultPng.length});
+  res.end(resultPng);
 };
 
 export default getPnl;
